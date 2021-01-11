559238fa9cd9ba2868b89602029125726c524c3f
Alejandro19921222 <niojapon@gmail.com>
2021-01-11 08:06:35 +0000

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '07c6efa4d8b8735e092dc830de90e53f0ee19b3bf45d38378c7517c7aca135955363537ce90019bff56ecbf0bf2b88c244000fde83f18904e8eee85f43546ac6'
